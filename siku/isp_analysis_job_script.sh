49c06f16cb7503c60011df9ec2012a7e623d20c3
victorialange <victoria@silverorange.com>
2024-07-23 20:08:25 -0300

    Add job script for analysis script loading dask distributed & more
    - loads more modules needed such as sklearn, seaborn, joblib

---
+++ siku/isp_analysis_job_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=00:15:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --account=an-tr043
+#SBATCH --output=isp_analysis_job_script_output.txt
+
+# add modules
+module load python/3.11.5
+
+# create virtual env to load necessary libraries/modules
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index pandas numpy matplotlib dask seaborn joblib sklearn
+pip install "dask[distributed]" --upgrade
+
+# run clean_dataset python script
+python analyze_dataset.py
